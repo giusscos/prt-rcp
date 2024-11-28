c4b6623f5c820b3b17ebe871da1311c58fd8c4e7
giusscos <giu.cosenza08@gmail.com>
2024-11-28 11:19:23 +0100

    Fix jumbo and add responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,29 @@
 <script setup lang="ts">
-import { Card, CardContent } from '@/components/ui/card'
-
 const isLoggedIn = useIsLoggedInUser()
 </script>
 
 <template>
   <div>
-    <section class="grid grid-cols-12 gap-6">
-      <Card class=" col-span-5 overflow-hidden">
-        <CardContent class="flex aspect-square items-center justify-center p-0">
-          <img src="https://picsum.photos/seed/picsum/700/900" alt="images shocase"
-            class="object-cover object-center w-full h-full block">
-        </CardContent>
-      </Card>
+    <section class="grid place-content-center px-10">
+      <div class="flex flex-wrap-reverse lg:flex-nowrap gap-6 p-10">
+        <div class="w-96 h-96 aspect-square bg-neutral-900 rounded-lg border border-neutral-500 flex justify-center items-center">
+          <span class="text-2xl p-6">Photo</span>
+        </div>
 
-      <div class="col-span-6 flex flex-col gap-4">
-        <h2 class="text-6xl font-serif font-bold">
-          Showcase your recipes and expand your audience 🌍
-        </h2>
-        <p class="text-2xl font-semibold">
-          Showcase your unique recipes and connect with a broader audience, inspire others, and turn wasted food
-          into culinary masterpieces.
-        </p>
-        <Button class="w-fit">
-          <NuxtLink :to="isLoggedIn ? '/dashboard' : '/login'">
-            {{ isLoggedIn ? 'Go to Dashboard' : 'Sign In' }}
-          </NuxtLink>
-        </Button>
+        <div>
+          <h2 class="text-4xl lg:text-6xl font-serif font-bold">
+            Showcase your recipes and expand your audience 🌍
+          </h2>
+          <p class="text-xl lg:text-2xl font-semibold py-4">
+            Showcase your unique recipes and connect with a broader audience, inspire others, and turn wasted food
+            into culinary masterpieces.
+          </p>
+          <Button class="w-fit">
+            <NuxtLink :to="isLoggedIn ? '/dashboard' : '/login'">
+              {{ isLoggedIn ? 'Go to Dashboard' : 'Sign In' }}
+            </NuxtLink>
+          </Button>
+        </div>
       </div>
     </section>
   </div>
